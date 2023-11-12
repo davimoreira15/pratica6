83b38bc38a255ac136825d2e56d674fe5796e9d8
Davi Moreira <143099293+davimoreira15@users.noreply.github.com>
2023-11-12 12:47:03 -0300

    Update Pessoa.java

--- src/exercicio2/Pessoa.java
+++ src/exercicio2/Pessoa.java
@@ -2,6 +2,14 @@ package exercicio2;
 
 public class Pessoa {
 
+    String nome;
+    String email;
+    String telefone;
 
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
 
 }
