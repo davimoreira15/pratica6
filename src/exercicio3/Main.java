9d6ecbd7f9de11f125d302bea398a71cd6f06fbc
Davi Moreira <143099293+davimoreira15@users.noreply.github.com>
2023-11-12 13:09:41 -0300

    Update Main.java

--- src/exercicio3/Main.java
+++ src/exercicio3/Main.java
@@ -4,8 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
 
+    ArrayList<Artigo> listaArtigos = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            Artigo artigo = new Artigo("Artigo " + i, "Resumo do Artigo " + i, "Autor " + i);
+            listaArtigos.add(artigo);
+        }
 
+        
+Edicao edicao = new Edicao(1, 1, "01/01/2023", 1000);
+        for (Artigo artigo : listaArtigos) {
+            edicao.adicionaArtigo(artigo);
+        }
 
+        RevistaCientifica revista = new RevistaCientifica("Revista de Ciências", 1234567890, "Mensal");
+        revista.adicionaEdicao(edicao);
 
 
 
