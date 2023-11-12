342d9e4d5d0af0600b50d51d2bffe9e6bc97aa8a
Davi Moreira <143099293+davimoreira15@users.noreply.github.com>
2023-11-12 12:52:18 -0300

    Update Empresa.java

--- src/exercicio2/Empresa.java
+++ src/exercicio2/Empresa.java
@@ -1,4 +1,29 @@
 package exercicio2;
 
 public class Empresa {
+
+    String nome;
+    String cnpj;
+    int qtdeDeFuncionarios;
+    Funcionario[] funcionarios;
+
+    public Empresa(String nome, String cnpj) {
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.qtdeDeFuncionarios = 0;
+        this.funcionarios = new Funcionario[100];
+    }
+
+ public void adicionarFuncionario(Funcionario funcionario) {
+        if (qtdeDeFuncionarios < 100) {
+            funcionarios[qtdeDeFuncionarios] = funcionario;
+            qtdeDeFuncionarios++;
+            System.out.println(funcionario.nome + " adicionado à empresa.");
+        } else {
+            System.out.println("limite de funcionários atingido.");
+        }
+    }
+
+
+  
 }
